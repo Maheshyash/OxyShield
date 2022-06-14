bb256433199b9e705acaa7044a74a0f7b667caec
TPSYS402\TP_USER <mahesh.ekambaram@talentpace.com>
2022-06-14 18:03:10 +0530

    qrcode scanning issue is resoved.

--- src/components/ScannerComponent/QrScanner.js
+++ src/components/ScannerComponent/QrScanner.js
@@ -7,66 +7,72 @@ import { RNCamera } from 'react-native-camera'
 import BarcodeMask from 'react-native-barcode-mask';
 import { useNavigation } from '@react-navigation/native';
 import { URL, URLSearchParams } from 'react-native-url-polyfill';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+// import { RNCamera } from 'react-native-camera';
 class QrScanner extends React.Component{
   constructor(props){
     super(props)
     this.camera=null
     this.state={
-      isError:false
+      isError:false,
     }
   }
   onFailure = () => {
-      this.setState({isError:true})
-      return Alert.alert(
-        "Alert",
-        "Invalid URL",
-        [
-          { text: "OK", onPress: () => this.setState({isError:false}) }
-        ]
-      );
-    }
+    this.setState({isError:true})
+    this.setState({isActive:false})
+    return Alert.alert(
+      "Alert",
+      "Invalid URL",
+      [
+        { text: "OK", onPress: () =>{ this.setState({isError:false})} }
+      ]
+    );
+  }
   onSuccess = e => {
+    this.setState({isError:true})
     if(e.hasOwnProperty('data')){
-      try{
+      if(!e.data.includes('otpauth://totp/Oxyshield'))return this.onFailure()
         var URLData = e.data;
-      var URLInfo = new URL(URLData);
-      const secret_key = URLInfo.searchParams.get('secret');
-      const application_name = URLInfo.searchParams.get('AppName');
-      const company_name = URLInfo.searchParams.get('appowner');
-      if(secret_key==null) return this.onFailure();
-      else if(application_name==null) return this.onFailure();
-      else if(company_name==null) return this.onFailure();
-      let userData = {
-        secret_key,
-        application_name,
-        company_name,
-      };
-      console.log(userData,'userData')
-      props.setSecretKeyUserData(userData);
-      this.props.navigation.navigate('Home'); 
-
-      }
-      catch(err){
-        console.log('invalid url')
-        this.setState({isError:true})
+        var URLInfo = new URL(URLData);
+        const secret_key = URLInfo.searchParams.get('secret');
+        const application_name = URLInfo.searchParams.get('AppName');
+        const company_name = URLInfo.searchParams.get('appowner');
+        let userData = {
+          secret_key,
+          application_name,
+          company_name,
+        };
+        console.log(userData,'userData')
+        if(secret_key==null) return this.onFailure();
+        else if(application_name==null) return this.onFailure();
+        else if(company_name==null) return this.onFailure();
+        // this.props.setSecretKeyUserData(userData);
         Alert.alert(
           "Alert",
-          "Invalid URL",
+          "Success",
           [
-            // {
-            //   text: "Cancel",
-            //   onPress: () => this.setState({isError:false}),
-            //   style: "cancel"
-            // },
-            { text: "OK", onPress: () => this.setState({isError:false}) }
+            { text: "OK", onPress: () =>{ this.setState({isError:false}),this.props.setSecretKeyUserData(userData),this.props.navigation.navigate('Home')} }
           ]
         );
-      }
+        // this.props.navigation.navigate('Home');
+        // this.setState({isError:true})
+        // Alert.alert(
+        //   "Alert",
+        //   "Invalid URL",
+        //   [
+        //     // {
+        //     //   text: "Cancel",
+        //     //   onPress: () => this.setState({isError:false}),
+        //     //   style: "cancel"
+        //     // },
+        //     { text: "OK", onPress: () => this.setState({isError:false}) }
+        //   ]
+        // );
     } 
+    
   };
-      
-      
   render(){
+    console.log(this.props,'this.props inside Qr Scanner')
     return(
       <View style={styles.container}>
         {this.state.isError ==! true && <RNCamera
@@ -74,7 +80,7 @@ class QrScanner extends React.Component{
           ref={ref => {
             this.camera = ref
           }}
-          onBarCodeRead={this.onSuccess}
+          onBarCodeRead={this.state.isError ==! true && this.onSuccess}
         >
           <BarcodeMask width={300} height={300} showAnimatedLine={true} outerMaskOpacity={0.5} animatedLineColor="green"/>
           <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
@@ -84,6 +90,22 @@ class QrScanner extends React.Component{
         </View>
         </RNCamera>}
       </View>
+      // <View style={{flex:1}}>
+      // <QRCodeScanner
+      //   onRead={this.onSuccess}
+      //   // flashMode={RNCamera.Constants.FlashMode.torch}
+      //   topContent={
+      //     <Text style={{fontSize:18,fontWeight:'600'}}>
+      //       Scan Qr-Code
+      //     </Text>
+      //   }
+      //   // bottomViewStyle={{display:'none'}}
+      //   // topViewStyle={{display:'none'}}
+      //   showMarker={true}
+      //   reactivate={this.state.isActive}
+      //   cameraTimeout={5000}
+      // />
+      // </View>
     )
   }
 }
